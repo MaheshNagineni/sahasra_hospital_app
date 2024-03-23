ab4a0fb4d25c7209c28d3fa30abcd0bf3b5b092d
mahesh nagineni <maheshb061@gmail.com>
2024-03-23 13:51:55 +0530

    minor tweaks

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,13 @@ import styles from "./page.module.css";
 import NavBar from "@/components/Navbar";
 import Carousel from "@/components/Carousel";
 import Examplenav from "@/components/Examplenav";
+import Gallery from "@/components/Gallery";
 
 export default function Home() {
   return <main className={styles.main}>
     {/* <NavBar /> */}
     <Examplenav />
     <Carousel />
+    <Gallery />
   </main>;
 }
