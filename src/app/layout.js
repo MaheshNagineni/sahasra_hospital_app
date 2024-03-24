b725c49f16383162c27abcad806344532a59da3e
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-24 15:18:09 +0530

    nav bug fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "./ui/components/Navbar";
+import dynamic from "next/dynamic";
+
+const NavBar = dynamic(() => import("./ui/components/Navbar"), { ssr: false });
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/ui/components/Navbar/index.js
+++ src/app/ui/components/Navbar/index.js
@@ -1,20 +1,33 @@
 "use client";
 
+import { useWindowSize } from "@/app/lib/useWindowSize";
 import Hamburger from "./Hamburger";
 import Logo from "./Logo";
 import Menu from "./Menu";
 import styles from "./Navbar.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
-  const [displayMenu, setDisplayMenu] = useState(true);
+  const { width } = useWindowSize();
+
+  const [displayMenu, setDisplayMenu] = useState(false);
+
+  useEffect(() => {
+    if (width >= 786 && displayMenu === false) setDisplayMenu(true);
+
+    return () => {};
+  });
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.nav}>
         <Logo />
         <Hamburger handleClick={() => setDisplayMenu(!displayMenu)} />
-        <div className={`${styles.menuWrapper} ${displayMenu ? "block" : "hidden"}`} >
+        <div
+          className={`${styles.menuWrapper} ${
+            displayMenu ? "block" : "hidden"
+          }`}
+        >
           <Menu />
         </div>
       </div>
