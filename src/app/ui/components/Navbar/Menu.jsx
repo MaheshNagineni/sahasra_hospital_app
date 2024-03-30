f744b06b1e3018dc231fbab468eae1dd707ea0a2
charan379 <37008444+charan379@users.noreply.github.com>
2024-03-30 19:04:30 +0530

    minor tweaks

--- src/app/ui/components/Navbar/Menu.jsx
+++ src/app/ui/components/Navbar/Menu.jsx
@@ -31,7 +31,7 @@ const navLinks = [
   { name: "Hospital & Doctors", url: "/#hospital" },
   { name: "Book Appoinment", url: "/#appoinment" },
   { name: "Gallery", url: "/#gallery" },
-  { name: "Contacts", url: "/#contact-us" },
+  { name: "Contacts", url: "/#about-us" },
   { name: "Follow Us", url: "/#follow-us" },
 ];
 
