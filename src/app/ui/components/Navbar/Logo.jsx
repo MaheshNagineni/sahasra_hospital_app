7acb7d9798202ceddc4bbb83ab4cb3f25c9538ac
charan379 <37008444+charan379@users.noreply.github.com>
2024-04-07 10:42:36 +0530

    nav image load in eager manner

--- src/app/ui/components/Navbar/Logo.jsx
+++ src/app/ui/components/Navbar/Logo.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <Link href="#home" className={styles.logo}>
-      <Image alt="Sahasra Hospitals" src={"/images/logo.png"} width="220" height="80" />
+      <Image alt="Sahasra Hospitals" src={"/images/logo.png"} width="220" height="80" loading="eager"/>
     </Link>
   );
 };
